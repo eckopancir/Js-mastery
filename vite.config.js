97c1d407f0620dece6c8a8f8b07eed61f1a06c00
eckopancir <55395245+eckopancir@users.noreply.github.com>
2026-03-18 09:10:08 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
+// Объединяем всё в один конфиг
 export default defineConfig({
   base: '/Js-mastery/', 
   plugins: [react()],
